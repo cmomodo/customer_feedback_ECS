9051229b1aa3d55c557c82a1e849a8dd7b8c5df4
cmomodo <ceesay.ml@outlook.com>
2026-01-27 11:32:18 +0000

    working on fixing the https. the redirect has been done with the listner. the next thing is now see how we can solve the problem with the img. the signup page isnt showing

--- infra/acm.tf
+++ infra/acm.tf
@@ -3,6 +3,48 @@ data "aws_route53_zone" "primary" {
   name = "ceedev.co.uk"
 }
 
+# SSL Certificate for HTTPS
+resource "aws_acm_certificate" "coderco_cert" {
+  domain_name       = "ceedev.co.uk"
+  subject_alternative_names = ["*.ceedev.co.uk"]
+  validation_method = "DNS"
+
+  tags = {
+    Name = "coderco-cert"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Certificate validation with DNS
+resource "aws_route53_record" "coderco_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.coderco_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.primary.zone_id
+}
+
+# Wait for certificate to be validated
+resource "aws_acm_certificate_validation" "coderco_cert" {
+  certificate_arn           = aws_acm_certificate.coderco_cert.arn
+  timeouts {
+    create = "5m"
+  }
+  depends_on = [aws_route53_record.coderco_cert_validation]
+}
+
 # A record pointing domain to ALB
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.primary.zone_id
