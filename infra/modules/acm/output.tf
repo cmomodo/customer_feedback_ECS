2c3d3c19736ea953de6f8160c378ce261f1e818f
cmomodo <ceesay.ml@outlook.com>
2026-02-17 11:56:06 +0000

    this is the updated output. just testing the pipeline while commenting the code.

--- infra/modules/acm/output.tf
+++ infra/modules/acm/output.tf
@@ -2,6 +2,7 @@ output "certificate_arn" {
   value = aws_acm_certificate.coderco_cert.arn
 }
 
+#the output for route53 zone id
 output "route53_zone_id" {
   value     = data.aws_route53_zone.primary.zone_id
   sensitive = true
