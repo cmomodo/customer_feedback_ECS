1ce57024c7c183ba7364ca53a4a6b92c758fb773
cmomodo <ceesay.ml@outlook.com>
2026-02-20 21:17:52 +0000

    my oidc was delete becuase i used resource now i am uisng data

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,32 +1,6 @@
-# GitHub OIDC Provider
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
-# GitHub OIDC Role for CI/CD
-resource "aws_iam_role" "github_oidc" {
+# Reference existing GitHub OIDC Role (managed manually)
+data "aws_iam_role" "github_oidc" {
   name = "github_oidc_portfolio"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = aws_iam_openid_connect_provider.github.arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringEquals = {
-          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-        }
-        StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
-        }
-      }
-    }]
-  })
 }
 
 resource "aws_cloudwatch_log_group" "app" {
