697144beed23090fedc3910a735400411eb03862
cmomodo <ceesay.ml@outlook.com>
2026-02-22 05:49:11 +0000

    added some comments and now pushing to github to test the oidc. it should work

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -99,7 +99,7 @@ resource "aws_route_table" "private_route_table" {
   }
 }
 
-
+#route table for private subnet
 resource "aws_route_table_association" "private_subnet_1_association" {
   subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_route_table.id
--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -1,33 +1,40 @@
+#the cidr block for vpc holder
 variable "cidr_block" {
   description = "CIDR block for the VPC"
   type        = string
 }
 
+#primary subnet output
 variable "primary_subnet" {
   description = "CIDR block for the primary public subnet"
   type        = string
 }
 
+#secondary subnet output
 variable "secondary_public_subnet" {
   description = "CIDR block for the secondary public subnet"
   type        = string
 }
 
+#private subnet cidr holder
 variable "private_subnet_1_cidr" {
   description = "CIDR block for the first private subnet"
   type        = string
 }
 
+#private subnet 2 holder
 variable "private_subnet_2_cidr" {
   description = "CIDR block for the second private subnet"
   type        = string
 }
 
+#the azs we will use
 variable "availability_zones" {
   description = "Availability zones to use (index 0 for primary, index 1 for secondary)"
   type        = list(string)
 }
 
+#we will create custom tags
 variable "tags" {
   description = "Common tags applied to all resources"
   type        = map(string)
