155c40962ee69bdbb1feb59924309956c2ed53cb
cmomodo <ceesay.ml@outlook.com>
2026-02-22 08:26:21 +0000

    cloudwatch improvements

--- infra/main.tf
+++ infra/main.tf
@@ -79,16 +79,6 @@ module "iam" {
 }
 
 
-import {
-  to = module.iam.aws_cloudwatch_log_group.app
-  id = "/ecs/my-app"
-}
-
-import {
-  to = module.iam.aws_iam_role.ecs_task_execution_role
-  id = "ecs_task_execution_role"
-}
-
 locals {
   database_url = "postgres://${module.secrets.db_username}:${module.secrets.db_password}@${module.rds.rds_endpoint}/${module.rds.db_name}"
 }
