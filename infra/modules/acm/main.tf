1938d9b4193d581713a03600aff6033988ccb329
cmomodo <ceesay.ml@outlook.com>
2026-04-26 08:03:36 +0100

    Import the certficate since its already issued with aws

--- infra/modules/acm/main.tf
+++ infra/modules/acm/main.tf
@@ -1,22 +1,6 @@
-# SSL Certificate for HTTPS
-resource "aws_acm_certificate" "coderco_cert" {
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-  validation_method         = "DNS"
-
-  tags = {
-    Name = "coderco-cert"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Wait for certificate to be validated
-resource "aws_acm_certificate_validation" "coderco_cert" {
-  certificate_arn = aws_acm_certificate.coderco_cert.arn
-  timeouts {
-    create = "5m"
-  }
+data "aws_acm_certificate" "coderco_cert" {
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  types       = ["AMAZON_ISSUED"]
+  most_recent = true
 }
--- infra/modules/acm/output.tf
+++ infra/modules/acm/output.tf
@@ -1,13 +1,8 @@
 output "certificate_arn" {
-  value = aws_acm_certificate.coderco_cert.arn
+  value = data.aws_acm_certificate.coderco_cert.arn
+  sensitive = true
 }
 
 output "domain_validation_options" {
-  value = {
-    for dvo in aws_acm_certificate.coderco_cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  value = {}
 }
