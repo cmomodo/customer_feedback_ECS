097d0d3f2dc4acfc6fd23091018bee801d383e17
cmomodo <ceesay.ml@outlook.com>
2026-05-05 10:05:47 +0100

    added sensitive and testing the pipeline

--- infra/modules/secrets/outputs.tf
+++ infra/modules/secrets/outputs.tf
@@ -3,7 +3,8 @@ output "db_username" {
 }
 
 output "db_password" {
-  value = local.db_password
+  value     = local.db_password
+  sensitive = true
 }
 
 output "db_name" {
@@ -11,11 +12,13 @@ output "db_name" {
 }
 
 output "db_identifier" {
-  value = local.db_identifier
+  value     = local.db_identifier
+  sensitive = true
 }
 
 output "task_secret_arn" {
-  value = aws_secretsmanager_secret.task_encrypt.arn
+  value     = aws_secretsmanager_secret.task_encrypt.arn
+  sensitive = true
 }
 
 output "jwt_secret_name" {
@@ -24,13 +27,16 @@ output "jwt_secret_name" {
 }
 
 output "jwt_secret_value" {
-  value = local.jwt_secret_value
+  value     = local.jwt_secret_value
+  sensitive = true
 }
 
 output "identifier_name" {
-  value = local.identifier_name
+  value     = local.identifier_name
+  sensitive = true
 }
 
 output "identifier_value" {
-  value = local.identifier_value
+  value     = local.identifier_value
+  sensitive = true
 }
